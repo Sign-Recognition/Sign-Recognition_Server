75ab682d33dd73698a30f04dd2db9115d599b06d
tiramisu <minjun13795@naver.com>
2020-12-29 21:30:47 +0900

    이제 완성인가?

--- Serv.py
+++ Serv.py
@@ -1,52 +1,41 @@
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
 import json
+import os
+import time
+from collections import OrderedDict
+
+import cv2
+import flask
+import werkzeug
 import numpy as np
-import base64
+from flask import request
 import tensorflow as tf
 import tensorflow_hub as hub
 import pandas as pd
-# compression
-import zlib
-import codecs
-
-
-app = Flask(__name__)
-api = Api(app)
-parser = reqparse.RequestParser()
-parser.add_argument('file', help = 'type error')
-
+from werkzeug import secure_filename
+app = flask.Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 model = tf.keras.models.load_model('model_31.h5',custom_objects={'KerasLayer':hub.KerasLayer})
 category_list=pd.Series(pd.read_csv('category_list_31.txt')['0'])
 
-class Predict(Resource):
-    def post(self):
-        data = parser.parse_args()
-        #print(data)
-        if data['file'] == "":
-            return {
-                    'data':'',
-                    'message':'No file found',
-                    'status':'error'
-                    }
+@app.route('/video', methods=['POST'])
+def handle_request():
+    print("\nRead Video ...")
+    video_file = flask.request.files["file"]
+    video_file.save(secure_filename(video_file.filename))
+    print("업로드 성공!")
 
-        #img = open(data['imgb64'], 'r').read() # doesn't work
-        img = data['file']
-        data2 = img.encode()
-        data2 = base64.b64decode(data2)
-        data2 = zlib.decompress(data2)
-        fdata = np.frombuffer(data2, dtype=np.float16)
-        fdata = fdata.reshape((1, 60, 224, 224, 3))
-        print(fdata)
-        #fdata = fdata[:, :, :, :, [2, 1, 0]]
-        # print(data)
-        pred=model(fdata)
-        pred=category_list[np.argmax(np.array(pred))]
-        print("Send Complete!\n")
-        return json.dumps(pred, ensure_ascii=False)
+    data = np.zeros((60, 224, 224, 3))
+    cap = cv2.VideoCapture('temp.avi')
 
+    for i in range(60):
+        ret, data[i] = cap.read()
+    cap.release()
+    data = data.reshape((1, 60, 224,224,3))
+    data /= 255.0
+    pred = model(data)
+    pred=category_list[np.argmax(np.array(pred))]
+    print("Send Complete!\n")
+    return json.dumps(pred, ensure_ascii=False)
 
-api.add_resource(Predict,'/video')
-
-if __name__ == '__main__':
-    app.run(debug=True, host = '0.0.0.0', port = 5000, threaded=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
